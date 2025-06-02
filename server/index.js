ef7f78b2fecc0ef3614ea5d0d30b671a23e2c3fd
SR01032006 <sarvesh01ramanan03@gmail.com>
2025-06-02 15:01:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,11 @@ mongoose
 // Use auth routes
 app.use("/api/auth", authRoutes);
 
+app.get('/', (req, res) => {
+    res.send('VentureXpert is running successfully!');
+});
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(Server is running on port ${PORT});
 });
